930b5a7418832b426deab81d896edfc44c717df2
Gustavo Santana <gustavossw@hotmail.com>
2023-07-27 13:06:06 -0300

    doc: google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ import Header from "../components/Header";
 
 import { ThemeProvider } from "next-themes";
 
+import Script from "next/script";
+
 export default function RootLayout({
   children,
 }: {
@@ -19,10 +21,22 @@ export default function RootLayout({
         <title>Portfólio - Gustavo Santana</title>
         <meta
           name="description"
-          content="
-        Olá, meu nome é Gustavo Santana, sou desenvolvedor web e apaixonado por tecnologia.
-        "
+          content="Olá, meu nome é Gustavo Santana, sou desenvolvedor web e apaixonado por tecnologia."
         />
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-XG85R89YFW"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-XG85R89YFW');
+        `}
+        </Script>
 
         <link rel="icon" href="/favicon.ico" />
       </head>
